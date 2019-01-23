8f5b2873900a2f25489692cc43033122e4aae3cf
patricklaptop <patrick@merklebros.com>
2019-01-23 13:31:57 -0600

    Add buildStops, buildFeedIds Server will now build a dictionary from stops.csv, and a dictionary for converting a line (ex "N"), to a feed_id (ex "16") to use in the get request to the MTA GTFS id.

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+const fs = require('fs'); 
+const parse = require('csv-parse')
 const GtfsRealtimeBindings = require('gtfs-realtime-bindings');
 const request = require('request');
+const dotEnv = require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const port = 3000;
-require('dotenv').config();
 
+//Route to get Mari's train time
 app.get('/', async (req, res) => {
   let time = await getNextTrainTime();
+  res.send(time.toString());
+})
+
+//Route to get next train times, where stations are stored on req.body.stations
+app.get('/next-train-times', async (req, res) => {
+  let stations = req.body.stations;
+  // let time = await getNextTrainTime(); TODO: Replace with general getNextTrainTime function
   res.send(time.toString());
 })
 
@@ -25,7 +36,7 @@ function getNextTrainTime() {
         let feed = GtfsRealtimeBindings.FeedMessage.decode(body);
         let arrivalTimes = [];
         feed.entity.forEach((entity) => {
-
+          
           let tripUpdate = entity.trip_update;
           if (tripUpdate && tripUpdate.trip.route_id ==="C") {
             tripUpdate.stop_time_update.forEach((update) => {
@@ -33,10 +44,10 @@ function getNextTrainTime() {
                   let time = update.arrival.time.low*1000;
                   arrivalTimes.push(time);
                 }
-            })
-          }
-
-        });
+              })
+            }
+            
+          });
         arrivalTimes.sort();
         let deltaTimes = formatArrivalTimes(arrivalTimes);
         if(deltaTimes[0] < 0 && deltaTimes[1]) {
@@ -47,9 +58,8 @@ function getNextTrainTime() {
         }
       }
     });
-
+    
   });
-
 }
 
 function formatArrivalTimes(arrivalTimes) {
@@ -57,9 +67,61 @@ function formatArrivalTimes(arrivalTimes) {
   arrivalTimes.forEach((time) => {
     let arrival = new Date(time);
     let deltaTime = Math.floor((arrival - Date.now())/60000);
-    console.log(deltaTime.toString());
     deltaTimes.push(deltaTime);
   });
-
+  
   return deltaTimes;
 }
+
+function buildStops() {
+  return new Promise(function(resolve, reject) {
+
+    let stops = {};
+    fs.createReadStream("stops.csv")
+        .pipe(parse({delimiter: ','}))
+        .on('data', function(csvrow) {
+          let stop = {
+            stationId: csvrow[0],
+            complexId: csvrow[1],
+            stopId: csvrow[2],
+            division: csvrow[3],
+            line: csvrow[4],
+            stopName: csvrow[5],
+            borough: csvrow[6],
+          }
+          stops[stop.stopId] = stop;
+        })
+        .on('end',function() {
+          resolve(stops);
+        });
+  });
+}
+
+function buildFeedIds() {
+  //NOTE S LINE IS ALSO INCLUDED IN ACEH DATA, include S in that array to see S data
+  let lineToFeedId = {};
+  ["1","2","3","4","5","6","S"].forEach((line) => {
+    lineToFeedId[line] = 1;
+  });
+  ["A","C","E","H"].forEach((line) => {
+    lineToFeedId[line] = 26;
+  });
+  ["N","Q","R","W"].forEach((line) => {
+    lineToFeedId[line] = 16;
+  });
+  ["B","D","F","M"].forEach((line) => {
+    lineToFeedId[line] = 21;
+  });
+  ["J", "M"].forEach((line) => {
+    lineToFeedId[line] = 36;
+  });
+  lineToFeedId["L"] = 2;
+  lineToFeedId["SIR"] = 11;
+  lineToFeedId["G"] = 31;
+  lineToFeedId["7"] = 51;
+
+  return lineToFeedId;
+}
+
+let stops = buildStops();
+let lineToFeedId = buildFeedIds();
